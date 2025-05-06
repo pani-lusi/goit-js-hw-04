cc1703b3dd771a8052727b722e0b570effbe5839
Liudmyla Panibratova <150482377+pani-lusi@users.noreply.github.com>
2025-05-06 15:26:10 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+  const bigNumbers = [];
+  for (const elem of numbers) {
+    if (elem > value) {
+      bigNumbers.push(elem);
+    }
+  }
+  return bigNumbers;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
